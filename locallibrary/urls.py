2df330de9f6fc9f8abf8f71ef8439fe16fd384c2
fahad-jamshad-ali <fahad@waltz-ai.com>
2025-07-31 18:20:58 +0500

    Added models inside catalog and some other fixes

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -15,13 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
-from django.urls import path, include
-from django.views.generic import RedirectView
-
 # Use static() to add URL mapping to serve static files during development (only)
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
